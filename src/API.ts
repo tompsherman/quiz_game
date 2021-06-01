734ef0f473d29a6e87607923f2ad729848b47040
unknown <tompsherman@gmail.com>
2021-06-01 16:41:03 -0400

    api logic

--- src/API.ts
+++ src/API.ts
@@ -1,1 +1,14 @@
 // logic for fetching data from API
+
+
+export enum Difficulty {
+    EASY = "easy",
+    MEDIUM = "medium",
+    HARD = "hard",
+}
+
+export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
+    const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+    const data = await(await fetch(endpoint)).json()
+    console.log(data)
+}
