a0aab14d39355499b6b3237d050e7467e61d0e88
unknown <tompsherman@gmail.com>
2021-06-02 16:15:30 -0400

    add scoreboard

--- src/App.tsx
+++ src/App.tsx
@@ -82,7 +82,7 @@ const App = () => {
         </button>
       ): null}
       
-      {!gameOver ? <p className="score">Score:</p> : null}
+      {!gameOver ? <p className="score">Score: {score}</p> : null}
       {loading && <p>Loading Questions...</p>}
       {!loading && !gameOver && (
       <QuestionCard
